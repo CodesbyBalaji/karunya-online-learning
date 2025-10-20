f976e986f7aa27e4d323d2aede287788b457a541
CodesbyBalaji <balajia@karunya.edu.in>
2025-10-20 22:04:29 +0530

    Added server.js modified one

--- root/server.js
+++ root/server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 // Change your require statement to:
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configure middleware
 
